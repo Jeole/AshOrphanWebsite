7d0209fd5a57cda0e67b8ae9faba7ba3a85f9715
jérémy Arruebarrena <jeremy.arruebarrena@gmail.com>
2018-07-17 11:25:49 +0200

    First early prototype basic structure, placeholders, etc

---
+++ js/main.js
@@ -0,0 +1,24 @@
+// Closes the Responsive Menu on Menu Item Click
+$('.navbar-collapse  a').click(function() {
+	console.log("test");
+    $('.navbar-toggler:visible').click();
+});
+
+ $("a.scroll-link").on('click', function(e) {
+
+       // prevent default anchor click behavior
+       e.preventDefault();
+		
+       // animate
+	   $(this.hash)[ 0 ].scrollIntoView({ behavior: 'smooth' });
+	   
+       // $('html, body').animate({
+           // scrollTop: $(this.hash).offset().top
+         // }, 300, function(){
+   
+           // // when done, add hash to url
+           // // (default click behaviour)
+           // window.location.hash = this.hash;
+         // });
+
+    });
